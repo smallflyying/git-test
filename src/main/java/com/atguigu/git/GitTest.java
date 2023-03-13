b8ebdd9dd141a0e0676e3a089f6dcbcc930202d3
LiHongFei <2529574569@qq.com>
2023-03-13 14:06:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LHF
+ * @create 2023-03-13 14:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
